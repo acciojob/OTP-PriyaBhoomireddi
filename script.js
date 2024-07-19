68169020438fd86e1804364b2ba8a90ec8a24fc6
Priya Bhoomireddi <priyagetlucky1011@gmail.com>
2024-07-19 15:38:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-//your JS code here. If required.
 const inputs = document.querySelectorAll('.code');
 
 inputs.forEach((input, index) => {
     input.addEventListener('keydown', (e) => {
-        if (e.key >= 0 && e.key <= 9) {
+        if (e.key >= '0' && e.key <= '9') {
             inputs[index].value = '';
             setTimeout(() => {
                 if (index < inputs.length - 1) {
@@ -11,12 +10,17 @@ inputs.forEach((input, index) => {
                 }
             }, 10);
         } else if (e.key === 'Backspace') {
-            if (index > 0 && inputs[index].value === '') {
-                setTimeout(() => {
-                    inputs[index - 1].focus();
-                }, 10);
+            if (inputs[index].value === '') {
+                if (index > 0) {
+                    setTimeout(() => {
+                        inputs[index - 1].focus();
+                    }, 10);
+                }
+            } else {
+                inputs[index].value = '';
             }
         }
     });
 });
 
+inputs[0].focus();
